c01c2b36bf056dfce64ca70900e821e039c8b6a2
DaR3k6 <kevinvilleperez@gmail.com>
2023-11-20 23:20:15 -0500

    API Terminada

---
+++ PROYECTKEY/Controllers/Detalle.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PROYECTKEY.Modelo;
+using System.Globalization;
+using System.Net;
+
+namespace PROYECTKEY.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    /// <summary>
+    /// Controlador para gestionar detalles de factura.
+    /// </summary>
+    public class Detalle : ControllerBase
+    {
+        /// <summary>
+        /// Agrega una factura y su detalle a la base de datos.
+        /// </summary>
+        /// <param name="fechaFactura"></param>
+        /// <param name="total"></param>
+        /// <param name="metodoPagoNombre"></param>
+        /// <param name="metodoPagoDescripcion"></param>
+        /// <param name="habilitado"></param>
+        /// <param name="producto_idProducto"></param>
+        /// <param name="cantidad"></param>
+        /// <param name="precioUnitario"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("facturaDetalle/agregarFacturaYDetalle")]
+        public ContentResult AgregarFacturaYDetalle(DateTime fechaFactura, decimal total, string metodoPagoNombre, string metodoPagoDescripcion, bool habilitado, int producto_idProducto, int cantidad, decimal precioUnitario)
+        {
+            try
+            {
+                // Llamar a la función para insertar la factura y el detalle
+                dynamic respuesta = ClaseDetalle.InsertarFacturaYDetalle(fechaFactura, total, metodoPagoNombre, metodoPagoDescripcion, habilitado, producto_idProducto, cantidad, precioUnitario);
+
+
+                // Crear una respuesta HTTP
+                var response = new ContentResult();
+
+                if (respuesta.status == true)
+                {
+                    // Código 200 si el registro es exitoso
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.Content = JsonConvert.SerializeObject(respuesta);
+                    response.ContentType = "application/json";
+                }
+                else
+                {
+                    // Código 400 si hay un error en el registro
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    response.Content = JsonConvert.SerializeObject(new { status = false, mensaje = "Error en la operación" });
+                    response.ContentType = "application/json";
+                }
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                return new ContentResult
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Content = JsonConvert.SerializeObject(new { status = false, mensaje = $"Error al insertar factura y detalle: {e.Message}" }),
+                    ContentType = "application/json"
+                };
+            }
+        }
+
+        /// <summary>
+        /// Elimina una sola factura
+        /// </summary>
+        /// <param name="facturaID"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("facturaDetalle/eliminarFacturaYDetalle/{facturaID}")]
+        public ContentResult EliminarFacturaYDetalle(int facturaID)
+        {
+            try
+            {
+                // Llamar a la función para eliminar factura y el detalle
+                dynamic respuesta = ClaseDetalle.EliminarFacturaDetalles(facturaID);
+
+
+                // Create an HTTP response
+                var response = new ContentResult();
+
+                if (respuesta.status == true)
+                {
+                    // Code 200 if the deletion is successful
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.Content = JsonConvert.SerializeObject(respuesta);
+                    response.ContentType = "application/json";
+                }
+                else
+                {
+                    // Code 400 if there is an error in the deletion
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    response.Content = JsonConvert.SerializeObject(new { status = false, mensaje = "Error en la operación" });
+                    response.ContentType = "application/json";
+                }
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                return new ContentResult
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Content = JsonConvert.SerializeObject(new { status = false, mensaje = $"Error al eliminar factura y detalle: {e.Message}" }),
+                    ContentType = "application/json"
+                };
+            }
+        }
+
+        /// <summary>
+        /// Actualizar la factura
+        /// </summary>
+        /// <param name="facturaID">ID de la factura a actualizar.</param>
+        /// <param name="nuevaFechaFactura"></param>
+        /// <param name="nuevoTotal"></param>
+        /// <param name="nombre"></param>
+        /// <param name="descripcion"></param>
+        /// <param name="habilitado"></param>
+        /// <param name="cantidad"></param>
+        /// <param name="precioUnitario"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("facturaDetalle/actualizarDetalleFacturaMetodos/{facturaID}")]
+        public ContentResult ActualizarDetalleFacturaMetodos(int facturaID, DateTime nuevaFechaFactura, decimal nuevoTotal, string nombre, string descripcion, bool habilitado, int cantidad, decimal precioUnitario)
+        {
+            try
+            {
+                dynamic respuesta = ClaseDetalle.ActualizarFacturaDetalleMetodoPago(facturaID, nuevaFechaFactura, nuevoTotal, nombre, descripcion, habilitado, cantidad, precioUnitario);
+
+                // Create an HTTP response
+                var response = new ContentResult();
+
+                if (respuesta.status == true)
+                {
+                    // Code 200 if the update is successful
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.Content = JsonConvert.SerializeObject(respuesta);
+                    response.ContentType = "application/json";
+                }
+                else
+                {
+                    // Code 400 if there is an error in the update
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    response.Content = JsonConvert.SerializeObject(new { status = false, mensaje = "Error en la operación" });
+                    response.ContentType = "application/json";
+                }
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                return new ContentResult
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Content = JsonConvert.SerializeObject(new { status = false, mensaje = $"Error al actualizar factura, detalle y método de pago: {e.Message}" }),
+                    ContentType = "application/json"
+                };
+            }
+        }
+        /// <summary>
+        /// Listar toda una factura
+        /// </summary>
+        /// <param name="facturaID"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("listarUnDetalleFacturaMetodos/{facturaID}")]
+        public ContentResult ListarUnDetalleFacturaMetodos(int facturaID)
+        {
+            try
+            {
+                // Call the function to list factura details and payment methods
+                dynamic respuesta = ClaseDetalle.ListarFacturasDetallesMetodosPago(facturaID);
+
+                // Create an HTTP response
+                var response = new ContentResult();
+
+                if (respuesta.status == true)
+                {
+                    // Code 200 if the operation is successful
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.Content = JsonConvert.SerializeObject(respuesta);
+                    response.ContentType = "application/json";
+                }
+                else
+                {
+                    // Code 400 if there is an error in the operation
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    response.Content = JsonConvert.SerializeObject(new { status = false, mensaje = "Error en la operación" });
+                    response.ContentType = "application/json";
+                }
+
+                return response;
+            }
+            catch (Exception e)
+            {
+                return new ContentResult
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Content = JsonConvert.SerializeObject(new { status = false, mensaje = $"Error al listar factura, detalle y método de pago: {e.Message}" }),
+                    ContentType = "application/json"
+                };
+            }
+        }
+    }
+}
---
+++ PROYECTKEY/Modelo/ClaseDetalle.cs
@@ -0,0 +1,291 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+
+namespace PROYECTKEY.Modelo
+{
+    public class ClaseDetalle
+    {
+        //CADENA DE CONEXION 
+        public static string cadena = "Server=(local)\\BDSQL;Database=PROYECT_KEY;User Id=kevin;Password=12345;";
+
+        public static string expecion = "";
+
+        //FUNCION PARA AGREGAR UN DETALLE, FACTURA Y EL METODO PAGO 
+        public static dynamic InsertarFacturaYDetalle(DateTime fechaFactura, decimal total, string metodoPagoNombre, string metodoPagoDescripcion, bool habilitado, int producto_idProducto, int cantidad, decimal precioUnitario)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(cadena))
+                {
+                    connection.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter("dbo.InsertarFacturaYDetalle", connection))
+                    {
+                        adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                        adapter.SelectCommand.Parameters.AddWithValue("@fechaFactura", fechaFactura);
+                        adapter.SelectCommand.Parameters.AddWithValue("@total", total);
+                        adapter.SelectCommand.Parameters.AddWithValue("@metodoPagoNombre", metodoPagoNombre);
+                        adapter.SelectCommand.Parameters.AddWithValue("@metodoPagoDescripcion", metodoPagoDescripcion);
+                        adapter.SelectCommand.Parameters.AddWithValue("@habilitado", habilitado);
+                        adapter.SelectCommand.Parameters.AddWithValue("@producto_idProducto", producto_idProducto);
+                        adapter.SelectCommand.Parameters.AddWithValue("@cantidad", cantidad);
+                        adapter.SelectCommand.Parameters.AddWithValue("@precioUnitario", precioUnitario);
+
+                        // Parámetros de salida
+                        SqlParameter resultadoParam = new SqlParameter("@resultado", SqlDbType.Bit)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+                        SqlParameter nuevoIDParam = new SqlParameter("@nuevoID", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+
+                        adapter.SelectCommand.Parameters.Add(resultadoParam);
+                        adapter.SelectCommand.Parameters.Add(nuevoIDParam);
+
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        int resultado = Convert.ToInt32(resultadoParam.Value);
+                        int nuevoID = Convert.ToInt32(nuevoIDParam.Value);
+
+                        if (resultado == 1)
+                        {
+                            Console.WriteLine("Inserción exitosa");
+                            return new
+                            {
+                                mensaje = "Inserción exitosa",
+                                status = true,
+                                detalle = new
+                                {
+                                    resultado = nuevoID,
+                                    fechaFactura,
+                                    total,
+                                    metodoPagoNombre,
+                                    metodoPagoDescripcion,
+                                    producto_idProducto,
+                                    cantidad,
+                                    precioUnitario
+                                }
+                            };
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error en la inserción");
+                            return new
+                            {
+                                mensaje = "Error en la inserción",
+                                status = false,
+                            };
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error al agregar el detalle: " + error.Message);
+                return new
+                {
+                    mensaje = "Error en la inserción",
+                    status = false,
+                    error = error.Message,
+                    nuevoID = (int?)null
+                };
+            }
+        }
+
+        //FUNCION DE ELIMAR LA FACTURA, DETALLE Y EL METODO DE PAGO
+        public static dynamic EliminarFacturaDetalles(int facturaID)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(cadena))
+                {
+                    connection.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter("dbo.EliminarFacturaDetallesYMetodoPago", connection))
+                    {
+                        adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                        adapter.SelectCommand.Parameters.AddWithValue("@facturaID", facturaID);
+                        adapter.SelectCommand.Parameters.Add("@resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        bool resultado = Convert.ToBoolean(adapter.SelectCommand.Parameters["@resultado"].Value);
+
+                        if (resultado == true)
+                        {
+                            return new
+                            {
+                                status = true,
+                                mensaje = "Eliminación exitosa",
+                                producto = new
+                                {
+                                    resultado,
+                                }
+                            };
+                        }
+                        else
+                        {
+                            return new
+                            {
+                                status = false,
+                                mensaje = "Error en la eliminación",
+
+                            };
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.Error.WriteLine("Error al eliminar la factura y sus detalles: " + error.Message);
+                return new
+                {
+                    status = false,
+                    mensaje = "Error en la eliminación",
+                    error = error.Message
+                };
+            }
+        }
+
+        public static dynamic ActualizarFacturaDetalleMetodoPago(int idFactura, DateTime nuevaFechaFactura, decimal nuevoTotal, string nombre, string descripcion, bool habilitado, int cantidad, decimal precioUnitario)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(cadena))
+                {
+                    connection.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter("dbo.ActualizarFacturaDetallesYMetodoPago", connection))
+                    {
+                        adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                        adapter.SelectCommand.Parameters.AddWithValue("@idFactura", idFactura);
+                        adapter.SelectCommand.Parameters.AddWithValue("@nuevaFechaFactura", nuevaFechaFactura);
+                        adapter.SelectCommand.Parameters.AddWithValue("@nuevoTotal", nuevoTotal);
+                        adapter.SelectCommand.Parameters.AddWithValue("@nombre", nombre);
+                        adapter.SelectCommand.Parameters.AddWithValue("@descripcion", descripcion);
+                        adapter.SelectCommand.Parameters.AddWithValue("@habilitado", habilitado);
+                        adapter.SelectCommand.Parameters.AddWithValue("@cantidad", cantidad);
+                        adapter.SelectCommand.Parameters.AddWithValue("@precioUnitario", precioUnitario);
+                        adapter.SelectCommand.Parameters.Add("@resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        bool resultado = Convert.ToBoolean(adapter.SelectCommand.Parameters["@resultado"].Value);
+
+                        if (resultado == true)
+                        {
+                            return new
+                            {
+                                status = true,
+                                mensaje = "Actualización exitosa",
+                                producto = new
+                                {
+                                    idFactura,
+                                    nuevaFechaFactura,
+                                    nuevoTotal,
+                                    nombre,
+                                    descripcion,
+                                    habilitado,
+                                    cantidad,
+                                    precioUnitario
+                                }
+                            };
+                        }
+                        else
+                        {
+                            return new
+                            {
+                                status = false,
+                                mensaje = "Error en la actualización",
+
+                            };
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.Error.WriteLine("Error al actualizar la factura, detalles y método de pago: " + error.Message);
+                return new
+                {
+                    status = false,
+                    mensaje = "Error en la actualización",
+                    error = error.Message
+                };
+            }
+        }
+
+        //FUNCION DE LISTRAR LA FACTURA, DETALLE Y LOS METODO DE PAGO
+        public static dynamic ListarFacturasDetallesMetodosPago(int facturaID)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(cadena))
+                {
+                    connection.Open();
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter("dbo.ListarFacturasDetallesMetodosPago", connection))
+                    {
+                        adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                        adapter.SelectCommand.Parameters.AddWithValue("@facturaID", facturaID);
+
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            List<object> detalles = new List<object>();
+
+                            foreach (DataRow row in dataTable.Rows)
+                            {
+                                detalles.Add(new
+                                {
+                                    idFactura = row["idFactura"],
+                                    fechaFactura = row["FechaFactura"],
+                                    total = row["Total"],
+                                    cantidad = row["Cantidad"],
+                                    precioUnitario = row["PrecioUnitario"],
+                                    nombre = row["Nombre"],
+                                    descripcion = row["Descripcion"],
+                                    habilitado = row["Habilitado"]
+                                });
+                            }
+
+                            return new
+                            {
+                                status = true,
+                                mensaje = "Lista de facturas, detalles y métodos de pago",
+                                detalle = new { detalles }
+                            };
+                        }
+                        else
+                        {
+                            return new
+                            {
+                                status = false,
+                                mensaje = "No se encontraron datos para la factura especificada",
+                                productos = new List<object>()
+                            };
+                        }
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                return new
+                {
+                    status = false,
+                    mensaje = "Error al buscar la factura",
+                    error = error.Message
+                };
+            }
+        }
+
+    }
+}
